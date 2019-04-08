41a57ad7f5dceaee16cbbbf7d376bf1a026ac982
Alexander DuPree <alexander.j.dupree@gmail.com>
2019-04-07 21:29:42 -0700

    General refactor and optmization
    User now lazily evaluates the following/followers list by not polling
    the GitHub API unless the lists are requested. The follower/following
    lists are now instantiated User objects which reduces the number of API
    calls. The graph object also holds a set of processed users to ensure
    that users are not processed twice.

--- GithubNetwork/helpers/GitHubAPI.py
+++ GithubNetwork/helpers/GitHubAPI.py
@@ -3,11 +3,12 @@ file: GitHubAPI.py
 
 brief: Defines a wrapper class for the GitHub RESTful API. Handles all HTTPS 
        requests and exception handling of those requests.
+
+https://github.com/AlexanderJDupree/GithubNetwork
 '''
 
 import os
 import requests
-from .user import User
 
 GITHUB_API = "https://api.github.com/"
 USER = os.environ['GITHUB_USER'] if 'GITHUB_USER' in os.environ.keys() else ''
@@ -29,10 +30,58 @@ def getUser(username):
 
     response = _API_Request(GITHUB_API + "users/" + username)
 
-    followers = _API_Request(response['followers_url'])
+    return User(response)
 
-    # Raw response has {/other user} optional at end or url
-    following = _API_Request(response['following_url'].split('{')[0])
+def getFollowers(followers_url):
 
-    return User(response, followers, following)
+    try:
+        followers = _API_Request(followers_url)
+    except requests.exceptions.HTTPError:
+        followers = []
+
+    return [User(follower) for follower in followers]
+
+class User:
+
+    ''' 
+    User class represents a GitHub user account. Holding data such as login, 
+    id, and a list of followers/following
+    '''
+
+    def __init__(self, data):
+        self._data = data
+        self._followers = None
+        self._following = None
+
+    def login(self):
+        return self._data['login']
+
+    def avatar_url(self):
+        return self._data['avatar_url']
+
+    def id(self):
+        return self._data['id']
+
+    def followers(self):
+        if(self._followers == None):
+            self._followers = getFollowers(self._data['followers_url'])
+        return self._followers
+
+    def following(self):
+        if(self._following == None):
+            # Raw response has {/other user} optional at end or url
+            self._following = getFollowers(self._data['following_url'].split('{')[0])
+        return self._following
+
+    def __repr__(self):
+        return "login: " + self.login() + " id: " + str(self.id())
+
+    def __str__(self):
+        return self.login()
+
+    def __eq__(self, other):
+        return self.id() == other.id()
+
+    def __hash__(self):
+        return self.id()
 
